c5de26a66be856f347b2771143ab280c308ede5e
songxiaoqi <wosongxiaoqi@163.com>
2019-05-29 17:43:56 +0800

    weis-erp  first commit

---
+++ src/main/java/com/song/erp/weiserp/WeisErpApplication.java
@@ -0,0 +1,13 @@
+package com.song.erp.weiserp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeisErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeisErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/song/erp/weiserp/WeisErpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.song.erp.weiserp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeisErpApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
